68e208654242f3cccca1cd8998104724735318dd
aslokaa <aslokaa@gmail.com>
2020-04-10 17:40:10 +0200

    Schijder van de verschillende dingen in de dataset

--- Assets/Scripts/Dataset/DatasetTest.cs
+++ Assets/Scripts/Dataset/DatasetTest.cs
@@ -11,7 +11,16 @@ namespace Assets.Scripts.Dataset
     class DatasetTest : MonoBehaviour
     {
         public List<string> listAll = new List<string>();
-
+        public List<string> listMisionId = new List<string>();
+        public List<string> listId = new List<string>();
+        public List<string> listQuestionType = new List<string>();
+        public List<string> listQuestion = new List<string>();
+        public List<string> listDidYouKnow = new List<string>();
+        public List<string> listDifficulty = new List<string>();
+        public List<string> listVerification = new List<string>();
+        public List<string> listComments = new List<string>();
+        public List<string> listAnswer = new List<string>();
+        public string text;
 
         private void Start()
         {
@@ -22,15 +31,55 @@ namespace Assets.Scripts.Dataset
 
                 while (!reader.EndOfStream)
                 {
-                    var line = reader.ReadToEnd();
-              
-                    var values = line.Split(';');
-                    
+                    text = reader.ReadToEnd();
+                    var values = text.Split(';');
+
+                    int type = 0;
+
+
+
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        if (values[i].Length != 0)
+                        {
+                            switch (type++)
+                            {
+                                case 0:
+                                    listMisionId.Add(values[i]);
+                                    break;
+                                case 1:
+                                    listId.Add(values[i]);
+                                    break;
+                                case 2:
+                                    listQuestionType.Add(values[i]);
+                                    break;
+                                case 3:
+                                    listQuestion.Add(values[i]);
+                                    break;
+                                case 4:
+                                    listDidYouKnow.Add(values[i]);
+                                    break;
+                                case 5:
+                                    listDifficulty.Add(values[i]);
+                                    break;
+                                case 6:
+                                    listVerification.Add(values[i]);
+                                    break;
+                                case 7:
+                                    listComments.Add(values[i]);
+                                    break;
+                                case 8:
+                                    listAnswer.Add(values[i]);
+                                    type = 0;
+                                    break;
+                            }
+                        }
+                    }
+
                     foreach (string value in values)
                     {
                         listAll.Add(value);
                     }
-
 
                 }
             }
@@ -40,7 +89,7 @@ namespace Assets.Scripts.Dataset
 
         private void Update()
         {
-          //Read(listAll);
+            //Read(listAll);
         }
 
         public void Read(List<string> list)
