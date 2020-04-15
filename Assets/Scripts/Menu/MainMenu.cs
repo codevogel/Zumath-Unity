b1dcb48e5c3c94a0142a097ba8285667664db3a7
DanielvdM <daniel.van.der.molen@hva.nl>
2020-04-15 18:38:34 +0200

    added menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(0); 
+    }
+    public void ExitGame ()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
