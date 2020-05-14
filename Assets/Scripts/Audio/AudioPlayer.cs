ac95df1317f8c828d94d788a8fb31783e68d9c38
aslokaa <aslokaa@gmail.com>
2020-05-14 13:09:44 +0200

    cannonshootin

---
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Audio
+{
+    class AudioPlayer : MonoBehaviour
+    {
+        public AudioSource audio;
+        
+
+        public void play()
+        {
+            if (!audio.isPlaying)
+            {
+                audio.Play();
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Audio/CannonAudio.cs
@@ -0,0 +1,27 @@
+﻿using States.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Audio
+{
+    class CannonAudio : AudioPlayer
+    {
+        private bool hasPlayed;
+
+        private void Update()
+        {
+            if (GameStateManager.GetGameState() == GameState.SHOOTING && !hasPlayed)
+            {
+                play();
+                hasPlayed = true;
+            }
+            if (GameStateManager.GetGameState() != GameState.SHOOTING)
+            {
+                hasPlayed = false;
+            }
+        }
+    }
+}
