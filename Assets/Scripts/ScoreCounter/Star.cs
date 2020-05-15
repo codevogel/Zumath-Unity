ef85ac6fa06ee414ea63aba5b32e551dfce1f412
Nick Wijker <nick_wijker@hotmail.com>
2020-05-15 12:28:36 +0200

    Star class met child stars gemaakt, nog niet af. Ook een starmanager met update functie die de verdiende sterren bijhoudt.

---
+++ Assets/Scripts/ScoreCounter/Star.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Resources;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Star : MonoBehaviour
+{
+    public bool zeroStarEnabled = false;
+    public bool oneStarEnabled = false;
+    public bool twoStarEnabled = false;
+    public bool threeStarEnabled = false;
+
+    public Image starOne, starTwo, starThree;
+
+    public static void Start()
+    {
+        GameObject.Find("StarSprite");
+        starOne.enabled = false;
+        starTwo.enabled = false;
+        starThree.enabled = false;
+    }
+}
+    public class ChildStarOne : Star
+    {
+        public void StarOne()
+        {
+        }
+    }
+    public class ChildStarTwo : Star
+    {
+        public void StarTwo()
+        {
+        }
+    }
+    
+
+    public class ChildStarThree : Star
+    {
+        public void StarThree()
+        {
+        }
+    }
+    
+
+
---
+++ Assets/Scripts/ScoreCounter/StarManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.ScoreCounter
+{
+    class StarManager : MonoBehaviour
+    {
+        void Start()
+        {
+            Star.Start();            
+        }
+
+        void Update()
+        {
+            if (ScoreAdd.score >= 1000 && ScoreAdd.score < 2000)
+            {
+                starOne.enabled = true;
+                oneStarEnabled = true;
+            }
+            else if (ScoreAdd.score >= 2000 && ScoreAdd.score < 3000)
+            {
+                starTwo.enabled = true;
+                twoStarEnabled = true;
+            }
+
+            else if (ScoreAdd.score >= 3000)
+            {
+                starThree.enabled = true;
+                threeStarEnabled = true;
+            }
+        }
+
+
+    }
+
+}
