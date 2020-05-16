71b6738cfb9c68e45f71678f6b708508c06cd23c
aslokaa <aslokaa@gmail.com>
2020-05-17 00:41:40 +0200

    checkpoiints on map

---
+++ Assets/Scripts/DevTools/CheckpointDevTool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.DevTools
+{
+    class CheckpointDevTool : MonoBehaviour
+    {
+        public bool checkpointsOn = true;
+        public int checkpointsTriggered = 0;
+
+        public bool TriggerCheckpoint()
+        {
+            checkpointsTriggered++;
+            return true;
+        }
+    }
+}
