a75e67d1bb6a9237dd1702913b67a1334bf85fcb
Nick Wijker <nick_wijker@hotmail.com>
2020-05-25 16:49:19 +0200

    De particles worden afgespeeld als de ster is verdient.

--- Assets/Scripts/ScoreCounter/StarManager.cs
+++ Assets/Scripts/ScoreCounter/StarManager.cs
@@ -1,16 +1,21 @@
-﻿using System;
+﻿using JetBrains.Annotations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 namespace Assets.Scripts.ScoreCounter
 {
     class StarManager : MonoBehaviour
     {
+        public ParticleSystem ps1, ps2, ps3;
         public Image OneStar, TwoStar, ThreeStar;
+        public bool particle1Played, particle2Played, particle3Played;
 
         void Start()
         {
@@ -20,20 +25,38 @@ namespace Assets.Scripts.ScoreCounter
         }
 
         void Update()
-        {
+        { 
             if (ScoreAdd.score >= 50)
             {
                 OneStar.enabled = true;
+
+                if (!particle1Played)
+                {
+                    ps1.Emit(50);
+                    particle1Played = true;
+                }
             }
 
             if (ScoreAdd.score >= 100)
             {
                 TwoStar.enabled = true;
+
+                if (!particle2Played)
+                {
+                    ps2.Emit(50);
+                    particle2Played = true;
+                }
             }
 
             if (ScoreAdd.score >= 200)
             {
                 ThreeStar.enabled = true;
+
+                if (!particle3Played)
+                {
+                    ps3.Emit(50);
+                    particle3Played = true;
+                }
             }
         }
     }
