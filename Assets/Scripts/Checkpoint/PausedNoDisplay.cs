45a1ec1f0daa83c4db55ccd9af53b2fd1a76eee2
aslokaa <aslokaa@gmail.com>
2020-05-28 17:55:29 +0200

    Line during paused

---
+++ Assets/Scripts/Checkpoint/PausedNoDisplay.cs
@@ -0,0 +1,25 @@
+﻿using States.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Checkpoint
+{
+    class PausedNoDisplay : MonoBehaviour
+    {
+        public LineRenderer line = null;
+
+        private void Update()
+        {
+            if (GameStateManager.GetGameState() == GameState.PAUSED)
+            {
+                line.enabled = false;
+            } else
+                line.enabled = true;
+        }
+
+    }
+}
