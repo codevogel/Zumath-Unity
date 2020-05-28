4e9be1098430cb60675fedfd00ac0af7ea54ac2d
aslokaa <aslokaa@gmail.com>
2020-05-28 14:14:18 +0200

    Score visible on loss screen

---
+++ Assets/Scripts/ScoreCounter/EndScreenScore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.ScoreCounter
+{
+    class EndScreenScore : MonoBehaviour
+    {
+        public TextMeshProUGUI textMesh = null;
+
+        private void Start()
+        {
+            textMesh.SetText("Score: " + ScoreAdd.score.ToString());
+        }
+    }
+}
