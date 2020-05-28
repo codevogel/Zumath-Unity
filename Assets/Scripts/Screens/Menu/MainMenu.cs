0a82ae15abf83ffe25b9f63ba5056b746768b736
aslokaa <aslokaa@gmail.com>
2020-05-28 15:55:35 +0200

    Instructions added

--- Assets/Scripts/Screens/Menu/MainMenu.cs
+++ Assets/Scripts/Screens/Menu/MainMenu.cs
@@ -5,11 +5,46 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame ()
+    public GameObject
+        tutorialText = null,
+        zumath = null,
+        exitButton = null,
+        playButton = null,
+        tutorialButton = null;
+
+    public void PlayGame()
     {
-        SceneManager.LoadScene(sceneName: "MainScene"); 
+        SceneManager.LoadScene(sceneName: "MainScene");
     }
-    public void ExitGame ()
+
+    public void DisplayTutorial()
+    {
+        tutorialText.SetActive(true);
+        zumath.SetActive(false);
+        exitButton.SetActive(false);
+        playButton.SetActive(false);
+        tutorialButton.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            NoDisplayTutorial();
+        }
+    }
+
+
+    public void NoDisplayTutorial()
+    {
+        tutorialText.SetActive(false);
+        zumath.SetActive(true);
+        exitButton.SetActive(true);
+        playButton.SetActive(true);
+        tutorialButton.SetActive(true);
+    }
+
+    public void ExitGame()
     {
         Debug.Log("Quitting game");
         Application.Quit();
