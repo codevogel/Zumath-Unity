eebc5882d0e75f490757bfc2e790b20d43c969cd
Kamiel <kamieldevisser@gmail.com>
2020-06-03 23:27:01 +0200

    Refactor and comment

--- Assets/Scripts/Followers/PathFollower.cs
+++ Assets/Scripts/Followers/PathFollower.cs
@@ -3,7 +3,6 @@ using PathCreation;
 using References;
 using Nodes;
 using Follower;
-using States.Game;
 
 // Moves along a path at constant speed.
 // Depending on the end of path instruction, will either loop, reverse, or stop at the end of the path.
@@ -14,30 +13,27 @@ namespace Following
     public class PathFollower : MonoBehaviour
     {
         public PathCreator pathCreator;
-        private NumberNode attachedNode;
         public EndOfPathInstruction endOfPathInstruction;
-        public bool following;
-        public bool forwards;
         public float distanceTravelled;
 
         void Awake()
         {
             pathCreator = GameObject.FindGameObjectWithTag(Tags.GUTTER).GetComponent<PathCreator>();
-            attachedNode = GetComponent<NumberNode>();
         }
 
         public void Follow(MoveType moveType)
         {
             float speed = NodeManager.GetSpeed();
-            switch (moveType)
+            // Adjust distance travelled
+            if (moveType == MoveType.FORWARD)
             {
-                case MoveType.FORWARD:
-                    distanceTravelled += speed * Time.deltaTime;
-                    break;
-                case MoveType.BACKWARD:
-                    distanceTravelled -= speed * Time.deltaTime;
-                    break;
+                distanceTravelled += speed * Time.deltaTime;
             }
+            else
+            {
+                distanceTravelled -= speed * Time.deltaTime;
+            }
+            // Update position accordingly
             transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
         }
 
