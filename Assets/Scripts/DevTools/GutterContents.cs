31add7067ead9754ab194e1a75ff86f404c2c2f9
Kamiel <kamieldevisser@gmail.com>
2020-06-03 23:25:58 +0200

    Refactor and comment

--- Assets/Scripts/DevTools/GutterContents.cs
+++ Assets/Scripts/DevTools/GutterContents.cs
@@ -6,10 +6,11 @@ using States.Game;
 
 namespace DevTools
 {
+    // Used to display the gameobjects that are in the gutter in the inspector view.
     public class GutterContents : MonoBehaviour
     {
 
-        public LinkedList<NumberNode> nodes = new LinkedList<NumberNode>();
+        public LinkedList<NumberNode> linkedNodeList = new LinkedList<NumberNode>();
         public List<NumberNode> nodeList = new List<NumberNode>();
 
         void Start()
@@ -28,8 +29,8 @@ namespace DevTools
 
         void RefreshGutter()
         {
-            nodes = NodeManager.GetNodes();
-            nodeList = nodes.ToList<NumberNode>();
+            linkedNodeList = NodeManager.GetNodes();
+            nodeList = linkedNodeList.ToList<NumberNode>();
         }
     }
 }
