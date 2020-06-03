e7842b8b86c022ddc1bb7891464807dafa8ebe31
Kamiel <kamieldevisser@gmail.com>
2020-06-03 23:28:21 +0200

    Refactor and comment

--- Assets/Scripts/Nodes/NodeSpawner.cs
+++ Assets/Scripts/Nodes/NodeSpawner.cs
@@ -6,14 +6,15 @@ using States.Node;
 using System;
 using States.Game;
 
+// This script is responsible for spawning the nodes that should fill the gutter at the start of the game and should only be used once
 public class NodeSpawner : MonoBehaviour
 {
 
     public GameObject numberBall;
     public Transform parentTransform;
     private PathCreator pathCreator;
-    public int nodesToSpawn;
 
+    public const int NODES_TO_SPAWN = 25;
     public const int INIT_DISTANCE = 20;
 
     void Awake()
@@ -22,15 +23,13 @@ public class NodeSpawner : MonoBehaviour
         SpawnNodes();
     }
 
-    private void Start()
-    {
-    }
-
+    // Spawn the nodes
+    // Called when a new game starts
     public void SpawnNodes()
     {
         // .75f is scale of object
-        float distanceTravelled = NumberNode.DIAMETER * 0.75f * nodesToSpawn - NumberNode.DIAMETER + INIT_DISTANCE;
-        for (int i = nodesToSpawn; i > 0; i--)
+        float distanceTravelled = NumberNode.DIAMETER * 0.75f * NODES_TO_SPAWN - NumberNode.DIAMETER + INIT_DISTANCE;
+        for (int i = NODES_TO_SPAWN; i > 0; i--)
         {
             SpawnNodeAtDistance(distanceTravelled);
             distanceTravelled -= NumberNode.DIAMETER;
@@ -39,13 +38,19 @@ public class NodeSpawner : MonoBehaviour
 
     private void SpawnNodeAtDistance(float distanceTravelled)
     {
+        // Get the coordinates of the point at the distance travelled
         Vector3 pointOnPath = pathCreator.path.GetPointAtDistance(distanceTravelled);
+        // Spawn a new node under the parent transform at those coordinates
         NumberNode newNode = Instantiate(numberBall, pointOnPath, Quaternion.identity, parentTransform).GetComponent<NumberNode>();
         newNode.Init();
+        // Set the pathfollower's distance travelled acoordingly
         newNode.pathFollower.SetDistanceTravelled(distanceTravelled);
+        // Set the node state to be in the gutter
         newNode.SetState(NodeState.GUTTER);
+        // Disable the node's motor as the pathfollower takes care of movement in the gutter
         newNode.nodeMotor.enabled = false;
         newNode.SetColorToValue();
+        // Add the node to the node manager
         NodeManager.AddNode(newNode);
     }
 }
